ab70ad2a228e111f401fee40e14b81ff56688ee3
Mike Garrity <mikergarrity@gmail.com>
2020-07-22 21:53:40 -0500

    added state and game functions

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Square from './Square';
 
+const style = {
+  border: '5px solid #31393E',
+  borderRadius: '10px',
+  width: '255px',
+  height: '250px',
+  margin: '0 auto',
+  display: 'grid',
+  gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+};
+
 const Board = ({ squares, onClick }) => {
   return (
-    <div>
-      <Square value="1" onClick={() => onClick("value here")} />
-      <Square value="2" onClick={() => onClick("value here")} />
-      <Square value="3" onClick={() => onClick("value here")} />
-      <Square value="4" onClick={() => onClick("value here")} />
-      <Square value="5" onClick={() => onClick("value here")} />
-      <Square value="6" onClick={() => onClick("value here")} />
-      <Square value="7" onClick={() => onClick("value here")} />
-      <Square value="8" onClick={() => onClick("value here")} />
-      <Square value="9" onClick={() => onClick("value here")} />
+    <div style={style}>
+      {squares.map((square, index) => (
+        <Square value={square} key={index} onClick={() => onClick(index)} />
+      ))}
     </div>
   )
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+const style = {
+  background: '#07122b',
+  color: '#fff',
+  border: '1px solid #282F33',
+  fontSize: '30px',
+  fontWeight: '800',
+  cursor: 'pointer',
+  outline: 'none',
+  margin: '0'
+
+}
 
 const Square = ({ onClick, value }) => {
   return (
-    <button onClick={onClick}>{value}</button>
+    <button style={style} onClick={onClick}>{value}</button>
   )
 }
 
