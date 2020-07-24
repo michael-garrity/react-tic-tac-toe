c7dce10bc868fce953994b803e364039b41b4bf1
Mike Garrity <mikergarrity@gmail.com>
2020-07-23 21:08:13 -0500

    adds time travel function

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,29 +8,41 @@ const styles = {
 }
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = (i) => {
-    const boardCopy = [...board];
-    if (winner || boardCopy[i]) return;
-    boardCopy[i] = xIsNext ? 'X' : 'O';
-    setBoard(boardCopy);
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
+    if (winner || squares[i]) return;
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
     setXisNext(!xIsNext);
   }
 
-  const jumpTo = () => {
-
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0)
   }
 
   const renderMoves = () => (
-    <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move#${move}` : `Go to start`;
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      )
+    })
   )
 
   return (
     <Fragment>
-      <Board squares={board} onClick={handleClick} />
+      <Board squares={history[stepNumber]} onClick={handleClick} />
       <div style={styles}>
         <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
         {renderMoves()}
